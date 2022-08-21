420ee5799787606f155fa79ad13dd8ad421666cf
EC2 Default User <ec2-user@ip-172-31-9-90.ec2.internal>
2022-08-21 16:28:57 +0000

    stopped at kubernates

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,20 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="chinoyerem/microproject:version1"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run microproject \
+    --image=chinoyerem/microproject:version1 \
+    --port=80 --labels app=microproject
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward microproject 8000:80
 
